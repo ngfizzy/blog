ace18ee523cb7375223a1d98d30c79c0fd012e3a
Bamidele Olufisayo <fisiwizy@gmail.com>
2019-06-11 03:55:06 +0100

    update material

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatMenuModule, MatIconModule,  MatToolbarModule } from '@angular/material';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { NavComponent } from './nav/nav.component';
 import { RouterModule } from '@angular/router';
 
