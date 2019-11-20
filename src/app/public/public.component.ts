3f7f6ff529a9d1c22ba6de4b6b01ec3b73d59850
Bamidele Olufisayo <fisiwizy@gmail.com>
2019-11-20 18:28:15 +0100

    ui cleanup for public pages

--- src/app/public/public.component.ts
+++ src/app/public/public.component.ts
@@ -10,9 +10,8 @@ export class PublicComponent implements OnInit {
     name: 'Olufisayo Bamidele',
     profession: 'Software Engineer and Writer',
     description: `
-      Hey There! Welcome to my blog. I write about variety of topics <br />
-      ranging from Tech, Self-help articles, Poems and any other thing my Muse
-      <br />
+      Hey There! Welcome to my blog. I write about variety of topics
+       ranging from Tech, Self-help articles, Poems and any other thing my Muse
       whispers.
     `
   };
