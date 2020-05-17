ad7172cbd2f1394a803d6384fdce15ae7c9c5ebf
Bamidele Olufisayo <fisiwizy@gmail.com>
2020-05-17 19:04:02 +0100

    show article created date on article component

--- src/app/mock-server/index.ts
+++ src/app/mock-server/index.ts
@@ -73,7 +73,7 @@ function generateArticle() {
     id,
     authorId: 1,
     published: randomPublishStatus(),
-    title: 'Lorem ipsum' + id,
+    title: 'Lorem ipsum ' + id,
     body: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sit amet vulputate quam. Pellentesque porta sollicitudin dui, in tincidunt metus tempor vitae. Sed pretium, ipsum nec gravida consectetur, sapien arcu bibendum orci, ac pellentesque lectus libero in mi. Praesent vulputate justo vel libero rutrum, et euismod sem iaculis. Mauris non erat vitae justo congue faucibus nec et leo. Morbi id porta neque. Vestibulum laoreet volutpat risus non hendrerit. Vestibulum sapien leo, varius quis finibus nec, iaculis eget lorem. In nec ex elit. Maecenas at finibus augue, eget feugiat odio. Ut vel ultricies ipsum.
     <br/>
     <br/>
@@ -241,7 +241,6 @@ export function findAudience(options: Partial<Audience>) {
     if (foundAudience) {
       foundAudience.email = email;
       foundAudience.audienceName = audienceName;
-
     }
   }
 
