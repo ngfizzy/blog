6fdd6792148c9a97d0af950c8acc47496a144e17
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-10-29 19:34:35 +0100

    ui adjustment for profile page

--- src/app/profile/containers/bio/bio.component.ts
+++ src/app/profile/containers/bio/bio.component.ts
@@ -3,11 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   template: `
   <div class="row author-info">
-    <p class="col-sm-12 col-md-8 text-center ml-auto mr-auto mb-5 ">
+    <p class="col-sm-12 col-md-8 text-center m-auto">
       Hello! M name is John Doe, a Software Developer and a writer based in Nigeria.
       When I'm not doing an of those things, netflix and chill.
     </p>
-
 
     <p class="col-sm-12 col-md-8 text-center m-auto">
       I am more experienced in Web Development with skills in Javascript/Node.js, PHP and frameworks
@@ -17,10 +16,10 @@ import { Component, OnInit } from '@angular/core';
   </div>
 `,
 styles: [`
-    .author-info p {
-        &:first-child {
-          margin-bottom: 1rem !important;
-      }
+    .author-info {
+      margin-top: 5rem;
+      font-size: 1.2rem;
+      height: 62%;
     }
   `],
 })
