2e1d114a73bf2884425cf0e2fa3020577b95881e
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-10-31 01:15:39 +0100

    add cosmetic changes to  profile page

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -3,9 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: 'profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
 })
 export class ProfileComponent implements OnInit {
+  expandedNav = false;
   constructor() { }
 
   ngOnInit() { }
