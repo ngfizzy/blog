abad82c3cb2e242cf3c68ca3edf4659b49bbaf7b
Olufisayo Bamidele <fisiwizy@gmail.com>
2020-11-15 18:47:20 +0100

    add cosmentic changes to bio route

--- src/app/profile/containers/bio/bio.component.ts
+++ src/app/profile/containers/bio/bio.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   template: `
   <div class="row author-info">
+    <h1 class="text-center col-12">Bio</h1>
     <h2 class="text-center col-12"><a class="text-center" [routerLink]="['/']">Olufisayo Bamidele</a></h2>
     <h3 class="col-12 text-center">Software Engineer</h3>
     <p class="col-sm-12 col-md-8 text-center m-auto">
@@ -15,8 +16,25 @@ import { Component, OnInit } from '@angular/core';
 styles: [`
     .author-info {
       margin-top: 5rem;
-      font-size: 1.5rem;
+      font-size: 1.2rem;
       height: 62%;
+    }
+
+    .author-info h1 {
+      font-size: 3rem;
+      text-decoration: underline;
+    }
+
+    .author-info h2 {
+      font-size: 2rem;
+    }
+
+    h2 a {
+      color: #fcd581;
+    }
+
+    .author-info h3 {
+      font-size: 1.5rem;
     }
   `],
 })
